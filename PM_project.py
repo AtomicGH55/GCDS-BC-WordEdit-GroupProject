e14b33e5c33b0d44e34c4c22d1bb9a6e441748a2
jonahsaidi <jsaidi24@gcds.net>
2022-10-28 09:11:36 -0400

    Uploaded Right Shift
    Jonah Upploaded Right shift

---
+++ PM_project.py
@@ -0,0 +1,29 @@
+'''
+Created on Oct 10, 2022
+
+@author: jsaidi24
+'''
+
+def RightShift(string, amount): # these are the variables im using, but the PM will make it so they work for the main 
+    
+    
+    shifted = '' # this is to show the string 
+    shifted = string[-amount:]
+    
+    amount_of_hashtags = (len(string) - len(shifted)) * "#" 
+    shifted = amount_of_hashtags + shifted 
+    
+    
+    
+    return shifted # the final result that we want 
+
+
+
+def main():
+    print(RightShift("somethingelse",5))
+    
+    
+    
+
+if __name__ == '__main__':
+    main()
