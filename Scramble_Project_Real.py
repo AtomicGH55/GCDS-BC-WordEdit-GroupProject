b2bbf41a7800ea1979cf8fccdf86e6f653e1eb2d
AtomicGH55 <bradycalhoun@gmail.com>
2022-10-28 08:57:09 -0400

    Updates to mistakes in Functions
    Made code cleaner in main and switched order of input data for circle functions.

--- Scramble_Project_Real.py
+++ Scramble_Project_Real.py
@@ -3,11 +3,13 @@ Created on Oct 18, 2022
 
 @author: BCalhoun24
 '''
-
+import RightCirc
+import LeftCirc                                                                                     #Brings in functions
+import reverse
 
 def main():
     
-    data_loop = True                                                                                        #sets variable for loop later
+    data_loop = True                                                                                #sets variable for loop later
     counter = 0
     
     print('LS = LeftShift')
@@ -18,7 +20,7 @@ def main():
     print('REV = Reverse')
     print('MC = ChaosWord')
     
-    while data_loop == True:
+    while data_loop == True:                                                                        #Asks for data input
         try:
             
             data_input = input('Input data here(Function-integer/word form): ')
@@ -27,39 +29,39 @@ def main():
             print('Input data in proper orientation.')
             continue
         
-        data_split = data_input.split("/")
+        data_split = data_input.split("/")                                                          #Begins seperation of full data input into list
         
-        variable_count = len(data_split)
+        variable_count = len(data_split)                                                            #Finds length of new list
         
-        running_str = data_split[variable_count-1]
+        running_str = data_split[variable_count-1]                                                  #Finds what the word/string being scrambled is
         
-        while counter < variable_count - 1:
+        while counter < variable_count - 1:                                                         #Begins loop
             
             function_split = data_split[counter].split("-")
             
-            if function_split[0] == ('LS'):
+            if function_split[0] == (LS):
                 
                 running_str = LeftShift(running_str , function_split[1])
                 
-            elif function_split[0] == ('RS'):
+            elif function_split[0] == (RS):
                 
                 running_str = RightShift(running_str , function_split[1])
                 
-            elif function_split[0] == ('LC'):
+            elif function_split[0] == (LC):
                 
                 running_str = LeftCirc(running_str , function_split[1])
                 
-            elif function_split[0] == ('RC'):
+            elif function_split[0] == 'RC':
                 
                 running_str = RightCirc(running_str , function_split[1])
                 
-            elif function_split[0] == ('REV'):
+            elif function_split[0] == 'REV':
                 
-                scramble_split = function_split[1].split("")
+                scramble_split = list(function_split[1])
                 
                 running_str = Reverse(running_str , scramble_split[0] , scramble_split[1])
                 
-            elif function_split[0] == ('MC'):
+            elif function_split[0] == 'MC':
                 
                 scramble_split = list(function_split[1])
                 
@@ -67,7 +69,8 @@ def main():
                 
             else:
                 print("DATA INPUT ERROR")
-                
+                return                                                                              #Fail safe for data issues
+
             data_loop = False
             
             counter = counter + 1
